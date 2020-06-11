1393506ff792775352c3cc138254eadbb852b9f9
jamesH-48 <60763364+jamesH-48@users.noreply.github.com>
2020-06-10 21:57:06 -0500

    Update part1.py

--- part1.py
+++ part1.py
@@ -1,38 +1,43 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import seaborn as sns
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import Normalizer, StandardScaler
+from sklearn import preprocessing
 
 def gradientdescent(x, y, weights, LR, iterations):
     # Graph MSE
     MSEgraph = np.zeros((iterations,1))
     for k in range(iterations):
-        if(iterations == 90000):
-            LR = .0000001
         # Initialize Hypothesis
         H = np.dot(x, weights)
+        #print("H",H.shape)
         # Define Error
         # E = H - Y
         E = np.subtract(H, y)
+        #print("E", E.shape)
         # Define Mean Squared Error
         MSE = (1 / (2 * (int(len(y))))) * np.dot(np.transpose(E), E)
+        #print("MSE", MSE.shape)
         MSEgraph[k] = MSE
         # print("MSE ", MSE)
         # Define Gradient -> MSE derivative to weight
-        gradient = (1 / (int(len(y)))) * np.dot(E, x)
+        gradient = (1 / (int(len(y)))) * np.dot(np.transpose(x), E)
+        #print("gradient", gradient.shape)
 
         # Revise Weights
         # New Weight = Old Weight - Learning Rate * Gradient
         weights = np.subtract(weights, LR * gradient)
     # Plot MSE
     print(MSEgraph)
-    print("Final Weights: ", weights)
+    print("Final Weights: \n", weights)
     fig2, ax = plt.subplots()
     ax.plot(MSEgraph)
     ax.set_title("Mean Squared Error")
     ax.set_xlabel("No. of Iterations")
     return weights
-
 
 # Attributes:
 # Cement, Blast Furnace Slag, Fly Ash, Water, Superplasticizer, Coarse Aggregate
@@ -41,25 +46,23 @@ def gradientdescent(x, y, weights, LR, iterations):
 # Retrieve Data from GitHub Repository
 url = "https://raw.githubusercontent.com/jamesH-48/Gradient-Descent-A1/master/Concrete_Data.csv"
 data = pd.read_csv(url, header=None)
+data = data.rename(columns={0:"Cement",1:"Blast Furnace Slag",2:"Fly Ash",3:"Water",4:"Superplasticizer",5:"Coarse Aggregate",6:"Fine Aggregate",7:"Age",8:"Concrete Compressive Strength"})
+
+# Add Column Intercept of 1s to data frame
+#data.insert(0, 'Intercept', 1)
+# So far hasn't changed value
+
 values = data.values
 
 '''
-Going to test this to see what leads to best results
+#Going to test this to see what leads to best results
 '''
-#  Pre-Processing ~ Remove Outliers from dataset
-# Over each column (except open/closed)
-'''for i in range(values.shape[1] - 1):
-	# Calculate Mean & Standard Deviation
-	Data_Mean, Data_STD = np.mean(values[:,i]), np.std(values[:,i])
-	# Define Outlier Boundary by the standard deviation
-	bound = Data_STD * 4
-	lower, upper = Data_Mean - bound, Data_Mean + bound
-	# Remove Outliers that are below the lower bound
-	below = [j for j in range(values.shape[0]) if values[j,i] < lower]
-	values = np.delete(values, below, 0)	# delete row if outlier
-	# Remove Outliers that are above the upper bound
-	above = [j for j in range(values.shape[0]) if values[j,i] > upper]
-	values = np.delete(values, above, 0)	# delete row if outlier'''
+# Compute pairwise correlation of columns
+corr = data.corr()
+# Display Heatmap of Correlations
+sns.set()
+axi1 = sns.heatmap(corr, cmap="BuPu", annot=True)
+#axi2 = sns.pairplot(data)
 
 # Plot Data ~ each column has its own subplot
 fig1 = plt.figure()
@@ -67,61 +70,96 @@ fig1.suptitle('Input Attributes', fontsize=16)
 for i in range(values.shape[1]):
     plt.subplot(values.shape[1], 1, i + 1)
     plt.plot(values[:, i])
-# Show Plot Graph of each attribute value vs number of attributes per column
-# plt.show()
 
-# Split Data based on Inputs vs Outputs for X and y
-Input = values[:, [0, 1, 2, 3, 4, 5, 6, 7]]
-Output = values[:, 8]
+#print(data.values[0])
+#data.to_csv('new.csv')
 
-'''
-Split Test
-A = np.array([[1,2],[1,2],[1,2],[3,4]])
-B = A[:int(len(A)*.5),:]
-C = A[int(len(A)*.5):,:]
-print(B,"\n",C)
-'''
+#  Pre-Processing ~ Remove Outliers from dataset
+# Over each column (except open/closed)
+removeList = []
+for i in range(values.shape[1] - 1):
+    # Calculate Mean & Standard Deviation
+    Data_Mean, Data_STD = np.mean(data.values[:,i]), np.std(data.values[:,i])
+    # Define Outlier Boundary by the standard deviation
+    bound = Data_STD * 4
+    lower, upper = Data_Mean - bound, Data_Mean + bound
+    # Remove Outliers that are below the lower bound
+    below = [j for j in range(data.values.shape[0]) if data.values[j,i] < lower]
+    if below:
+        for j in range(len(below)):
+            removeList.append(below[j]) # Append Row to Remove to Remove List
+    # Remove Outliers that are above the upper bound
+    above = [j for j in range(data.values.shape[0]) if data.values[j,i] > upper]
+    if above:
+        for j in range(len(above)):
+            removeList.append(above[j]) # Append Row to Remove to Remove List
+# Remove Duplicates if they exist
+removeList = list(set(removeList))
+# Sort Remove List in increasing order
+removeList.sort()
+print("length: ", len(removeList))
+# Remove Rows for Data Frame
+for i in range(len(removeList)):
+    data = data.drop(removeList[i])
 
-# Split Data based on Training/Test -> 80/20
-# X - attributes
-InputTrain = Input[:int(len(Input) * .8), :]
-InputTest = Input[int(len(Input) * .8):, :]
-# Y
-OutputTrain = Output[:int(len(Output) * .8)]
-OutputTest = Output[int(len(Output) * .8):]
+#data.to_csv('new.csv')
+
+# Train-Test Split Dataset
+# Create X data frame that contains the inputs
+Xdf = data[["Cement","Blast Furnace Slag","Fly Ash","Water","Superplasticizer","Coarse Aggregate","Fine Aggregate","Age"]]
+# Convert X data frame to numpy array (just known as X)
+X = Xdf.to_numpy()
+# Create Y data frame that contains the output
+Ydf = data[["Concrete Compressive Strength"]]
+# Convert Y data frame to numpy array (just known as Y)
+Y = Ydf.to_numpy()
+
+
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.2, random_state=0)
+
+
+# Proof of Shape
+print("X_train trans: ", np.transpose(X_train).shape)
+print("X_train: ", X_train.shape)
+print("X_test: ", X_test.shape)
+print("Y_train: ", Y_train.shape)
+print("Y_test: ", Y_test.shape)
+
 # Initialize Weights
-Weights = np.random.uniform(0.0, .3, size=8)
+#Weights = np.random.uniform(0.0, .2, size=8)
+Weights = np.array([[0],[0],[0],[0],[0],[0],[0],[0]])
+print("Weights: ", Weights)
+print(Weights.shape)
+
 # Initialize Learning Rate
 LR = .000001
 # Set Iterations
-iterations = 100000
+iterations = 10000
 
 '''
-Gradient Descent
+#Gradient Descent
 '''
-FinalWeights = gradientdescent(InputTrain, OutputTrain, Weights, LR, iterations)
-#plt.show()
+FinalWeights = gradientdescent(X_train, Y_train, Weights, LR, iterations)
+print(FinalWeights.shape)
+print(FinalWeights)
+# Weights Bar Graph
 
 # Apply Model to Test Data Set
 # Get X Values from Test Data x Weights Found
 # Compare to X Values with actual output values from test data set
-Y_pred1 = np.dot(InputTrain, FinalWeights)
-Y_pred2 = np.dot(InputTest, FinalWeights)
-'''correlation_matrix = np.corrcoef(X_values, OutputTest)
-correlation_xy = correlation_matrix[0, 1]
-r_squared = correlation_xy ** 2
-print(r_squared)'''
+Y_pred1 = np.dot(X_train, FinalWeights)
+Y_pred2 = np.dot(X_test, FinalWeights)
 
-print(mean_squared_error(Y_pred1,OutputTrain))
-print(r2_score(Y_pred1,OutputTrain))
+print(mean_squared_error(Y_pred1,Y_train))
+print(r2_score(Y_pred1,Y_train))
 print('\n')
-print(mean_squared_error(Y_pred2,OutputTest))
-print(r2_score(Y_pred2,OutputTest))
+print(mean_squared_error(Y_pred2,Y_test))
+print(r2_score(Y_pred2,Y_test))
 # Print Plot of Outputs
 figure1, ax = plt.subplots()
 figure2, ax2 = plt.subplots()
-ax.plot(OutputTest, color='black', markersize=5)
-ax.plot(Y_pred2, color='magenta', markersize=5)
-ax2.plot(OutputTrain, color='red', markersize=5)
-ax2.plot(Y_pred1, color='cyan', markersize=5)
+ax.plot(Y_train, color='red', markersize=5)
+ax.plot(Y_pred1, color='cyan', markersize=5)
+ax2.plot(Y_test, color='black', markersize=5)
+ax2.plot(Y_pred2, color='magenta', markersize=5)
 plt.show()
